87abe515c7e159ca0d3eeacf37ced8f07890d241
Ale192837 <alexandre.2018@alunos.utfpr.edu.br>
2023-01-31 17:57:14 -0300

    house keeping

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -50,12 +50,10 @@ function HomePage() {
 
 
 
-			{/* <Carousel /> */}
 			<div className="bg-neutral-900 w-full p-3 ">
 				<button onClick={() => portfoliosRef.current.scrollIntoView({behavior: "smooth"})} className="btn">Confira nossos portfólios</button>
 			</div>
 
-			{/* <div className="pt-20 lg:flex-row md:flex-col xl:w-full 2xl:w-4/5 xl:pl-10 xl:pr-10 flex  justify-center "> */}
 			<div className="pt-20 lg:flex-row md:flex-col xl:w-full 2xl:ml-32 2xl:mr-32 xl:pl-10 xl:pr-10 md:pl-3 md:pr-3 flex  justify-center ">
 
 				<div className=" text-left p-5 mt-5 m-2">
@@ -88,17 +86,6 @@ function HomePage() {
 
 
 
-			{/* <div className="grid grid-cols-1 grid-flow-row-dense gap-y-20 md:p-10 2xl:p-20">
-				<h2 className="text-left text-5xl font-semibold w-full ">Confira nossos portfolios</h2>
-					<CardViewLarge img={img_1} color={"bg-orange-700"} href={"https://www.artstation.com/alecremer"} label="Portfolio de artes em geral"/>
-					<CardViewLarge img={img_2} color={"bg-slate-700"} href={"https://www.artstation.com/alecremer"} label="Artes 3D"/>
-					<CardViewLarge img={img_5} color={"bg-slate-700"} href={"https://www.artstation.com/alecremerdev"} label="Desenvolvimento de games"/>
-					<CardViewLarge img={img_3} color={"bg-slate-700"} href={"https://www.artstation.com/alecremer"} label="Pixel art"/>
-					<CardViewLarge img={img_4} color={"bg-slate-700"} href={"https://www.artstation.com/alecremer"} label="Ilustrações"/>
-
-				
-
-			</div> */}
 			<div ref={portfoliosRef} className="grid grid-rows-2 grid-flow-row 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-1 md:gap-y-3  gap-10 2xl:p-20 xl:p-10 md:p-2">
 
 				<div className="flex md:justify-center flex-col pl-3">
@@ -121,10 +108,8 @@ function HomePage() {
 
 			<div className="border-t-2 text-left p-5 mt-5 flex justify-center w-full">
 
-
-				{/* <h2 className="bg-orange-600 text-white p-3 rounded-full font-semibold text-2xl text-center">Confira nossos serviços</h2> */}
-				{/* <button className="btn">Confira nossos serviços</button> */}
 				<h2 >2023 - © Alexandre Cremer Fonseca</h2>
+
 			</div>
 
 
